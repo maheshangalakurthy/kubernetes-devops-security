5f3a6696a0f743f4362241c87355965742235dee
maheshangalakurthy <angalakurthy.mahesh@gmail.com>
2023-09-04 17:13:43 -0400

    Adding the trivy scan

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -6,12 +6,14 @@ echo $dockerImageName
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.45.0 -q image --severity HIGH --exit-code 0 --light $dockerImageName
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.45.0 -q image --severity CRITICAL --exit-code 1 --light $dockerImageName
 
-exit_code=$?
-echo "Exit Code : $exit_code"
+  # Trivy scan result processing
+    exit_code=$?
+    echo "Exit Code : $exit_code"
 
-if [[ "${exit_code}" == 1]]; then
-  echo "Image Scanning failed. Vulnerabilties found"
-  exit 1;
-else
-  echo "Images scanning passed. No vulnerabilities found"
-fi
+    # Check scan results
+    if [[ "${exit_code}" == 1 ]]; then
+        echo "Image scanning failed. Vulnerabilities found"
+        exit 1;
+    else
+        echo "Image scanning passed. No CRITICAL vulnerabilities found"
+    fi;
