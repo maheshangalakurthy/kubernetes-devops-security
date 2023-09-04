693ad31573043906f4826ae29169d3546b0194d7
maheshangalakurthy <angalakurthy.mahesh@gmail.com>
2023-09-04 19:13:01 -0400

    Adding Trivy scan for images before deploying to k8s

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -3,8 +3,11 @@
 
 echo $imageName #getting Image name from env variable
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
+# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
+# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
+
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.45.0 -q image --severity LOW,MEDIUM,HIGH --exit-code 0 --light $imageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.45.0 -q image --severity CRITICAL --exit-code 1 --light $imageName
 
     # Trivy scan result processing
     exit_code=$?
