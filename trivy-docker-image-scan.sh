d7987cd09fff6114bc2a30e67c79c179687aee19
maheshangalakurthy <angalakurthy.mahesh@gmail.com>
2023-11-17 18:38:16 -0600

    trivy scan result eith exit code

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -3,8 +3,8 @@
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy image --severity CRITICAL --exit-code 1 $dockerImageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy image --severity HIGH --exit-code 0 $dockerImageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock $WORKSPACE:/root/.cache/ aquasec/trivy image --severity CRITICAL --exit-code 1 $dockerImageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock $WORKSPACE:/root/.cache/ aquasec/trivy image --severity HIGH --exit-code 0 $dockerImageName
 
  exit_code=$?
  echo "Exit Code : $exit_code"
